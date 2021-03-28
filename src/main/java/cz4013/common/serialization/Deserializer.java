048a8260ded01a95ebf97c07d2a834a7a66f2b9b
Mohamed Fahadh <mohamedfahadh1@hotmail.com>
2021-03-29 01:15:35 +0800

    refactor serialization

--- src/main/java/cz4013/common/serialization/Deserializer.java
+++ src/main/java/cz4013/common/serialization/Deserializer.java
@@ -16,87 +16,61 @@ import static cz4013.common.serialization.Utils.serializableFields;
 
 public class Deserializer {
   public static Map<String, Class> EMPTY_TYPE_MAP = new HashMap<>();
-
-  /**
-   * Deserializes an object from a buffer.
-   * <p>
-   * Note that to support both generic and non-generic classes,
-   * the given object must be created with anonymous class syntax due to type erasure.
-   * <p>
-   * For example:
-   * <pre>{@code
-   * class Clazz<T> {}
-   *
-   * // INCORRECT
-   * deserialize(new Clazz<Integer>(), buffer);
-   *
-   * // Correct (note the extra {})
-   * deserialize(new Clazz<Integer>() {}, buffer);
-   * }</pre>
-   *
-   * @param obj the object to be deserialized into
-   * @param buf the buffer to read from
-   * @param <T> the object type
-   * @return the deserialized object
-   * @throws SerializingException if the data is corrupted or the object's class violates the
-   *                              auto serialization constraints
-   */
+  
   public static <T> T deserialize(T obj, ByteBuffer buf) {
     buf.order(ByteOrder.LITTLE_ENDIAN);
     buf.clear();
     try {
-      Class<?> clazz = obj.getClass();
-      assert clazz.isAnonymousClass() : "The given object must be created with anonymous class syntax.";
-      return (T) readStruct(clazz.getGenericSuperclass(), buf, EMPTY_TYPE_MAP);
+      Class<?> currClass = obj.getClass();
+      assert currClass.isAnonymousClass() : "The given object must be created with anonymous class syntax.";
+      return (T) inputStruct(currClass.getGenericSuperclass(), buf, EMPTY_TYPE_MAP);
     } catch (BufferOverflowException e) {
-      throw new SerializingException("Corrupted data", e);
+      throw new SerializingException("Data is corrupted", e);
     }
   }
 
-  public static Object readStruct(Type ty, ByteBuffer buf, Map<String, Class> parentTypeMap) {
-    Class<?> clazz;
+  public static Object inputStruct(Type type, ByteBuffer buffer, Map<String, Class> parentTypeMap) {
+    Class<?> currClass;
     Map<String, Class> typeMap = EMPTY_TYPE_MAP;
-    if (ty instanceof ParameterizedType) {
-      ParameterizedType pty = (ParameterizedType) ty;
-      clazz = ((Class) pty.getRawType());
+    if (type instanceof ParameterizedType) {
+      ParameterizedType pty = (ParameterizedType) type;
+      currClass = ((Class) pty.getRawType());
       typeMap = makeTypeMap(pty, parentTypeMap);
     } else {
-      clazz = (Class<?>) ty;
+      currClass = (Class<?>) type;
     }
 
-    Object obj = newInstance(clazz);
+    Object obj = currInstance(currClass);
     if (obj instanceof Serializable) {
-      ((Serializable) obj).deserialize(buf);
+      ((Serializable) obj).deserialize(buffer);
     } else {
       Map<String, Class> finalTypeMap = typeMap;
-      serializableFields(clazz)
-        .forEach(field -> read(obj, field, buf, finalTypeMap));
+      serializableFields(currClass)
+        .forEach(field -> read(obj, field, buffer, finalTypeMap));
     }
 
     return obj;
   }
 
-  private static <T> T newInstance(Class<T> clazz) {
+  private static <T> T currInstance(Class<T> currClass) {
     try {
-      return clazz.newInstance();
+      return currClass.newInstance();
     } catch (InstantiationException e) {
-      assert false : "Cannot create an instance of " + clazz.getName() + ". " +
-        "Make sure the class is concrete and has a nullary constructor.";
+      assert false : "Unable to create an instance, " + currClass.getName() + ". Make sure class indicated is not abstract and has a 0-argument constructor.";
     } catch (IllegalAccessException e) {
-      assert false : "Unable to access " + clazz.getName() +
-        ". Make sure the class is declared public.";
+      assert false : "Unable to access " + currClass.getName() + ". Ensure that class is public.";
     }
 
-    throw new RuntimeException("We should never reach here.");
+    throw new RuntimeException("!!!!");
   }
 
-  public static Object read(Type ty, ByteBuffer buf, Map<String, Class> typeMap) {
-    if (ty == Byte.TYPE || ty == Byte.class) {
-      return buf.get();
+  public static Object readInput(Type type, ByteBuffer buffer, Map<String, Class> typeMap) {
+    if (type == Byte.TYPE || type == Byte.class) {
+      return buffer.get();
     }
 
-    if (ty == Boolean.TYPE || ty == Boolean.class) {
-      Byte b = buf.get();
+    if (type == Boolean.TYPE || type == Boolean.class) {
+      Byte b = buffer.get();
       switch (b) {
         case 1:
           return true;
@@ -106,158 +80,143 @@ public class Deserializer {
 
         default:
           throw new SerializingException(
-            String.format("Unexpected byte %d while reading a bool value at offset %d", b, buf.position())
+            String.format("Unexpected byte, %d when reading a bool value at offset %d", b, buffer.position())
           );
       }
     }
 
-    if (ty == Short.TYPE || ty == Short.class) {
-      return buf.getShort();
+    if (type == Short.TYPE || type == Short.class) {
+      return buffer.getShort();
     }
 
-    if (ty == Character.TYPE || ty == Character.class) {
-      return buf.getChar();
+    if (type == Character.TYPE || type == Character.class) {
+      return buffer.getChar();
     }
 
-    if (ty == Integer.TYPE || ty == Integer.class) {
-      return buf.getInt();
+    if (type == Integer.TYPE || type == Integer.class) {
+      return buffer.getInt();
     }
 
-    if (ty == Float.TYPE || ty == Float.class) {
-      return buf.getFloat();
+    if (type == Float.TYPE || type == Float.class) {
+      return buffer.getFloat();
     }
 
-    if (ty == Long.TYPE || ty == Long.class) {
-      return buf.getLong();
+    if (type == Long.TYPE || type == Long.class) {
+      return buffer.getLong();
     }
 
-    if (ty == Double.TYPE || ty == Double.class) {
-      return buf.getDouble();
+    if (type == Double.TYPE || type == Double.class) {
+      return buffer.getDouble();
     }
 
-    if (ty == String.class) {
-      int len = buf.getInt();
+    if (type == String.class) {
+      int len = buffer.getInt();
       byte[] utf8 = new byte[len];
-      buf.get(utf8);
+      buffer.get(utf8);
       return new String(utf8, StandardCharsets.UTF_8);
     }
 
-    if (ty == UUID.class) {
-      return new UUID(buf.getLong(), buf.getLong());
+    if (type == UUID.class) {
+      return new UUID(buffer.getLong(), buffer.getLong());
     }
 
-    if (ParameterizedType.class.isAssignableFrom(ty.getClass())) {
-      ParameterizedType pty = (ParameterizedType) ty;
-      Class<?> clazz = (Class<?>) pty.getRawType();
+    if (ParameterizedType.class.isAssignableFrom(type.getClass())) {
+      ParameterizedType pty = (ParameterizedType) type;
+      Class<?> currClass = (Class<?>) pty.getRawType();
 
-      if (Optional.class.isAssignableFrom(clazz)) {
-        Byte b = buf.get();
+      if (Optional.class.isAssignableFrom(currClass)) {
+        Byte b = buffer.get();
         switch (b) {
           case 0:
             return Optional.empty();
 
           case 1:
-            return Optional.of(read(
+            return Optional.of(readInput(
               resolveGenericType(pty.getActualTypeArguments()[0], typeMap),
-              buf,
+              buffer,
               typeMap
             ));
 
           default:
             throw new SerializingException(
-              String.format("Unexpected tag %d while reading an optional value at offset %d", b, buf.position())
+              String.format("Unexpected tag %d while reading an optional value at offset %d", b, buffer.position())
             );
         }
       }
 
-      if (Collection.class.isAssignableFrom(clazz)) {
-        Collection<Object> xs = ((Collection<Object>) newInstance(clazz));
-        int len = buf.getInt();
-        int pos = buf.position();
+      if (Collection.class.isAssignableFrom(currClass)) {
+        Collection<Object> s = ((Collection<Object>) currInstance(currClass));
+        int length = buffer.getInt();
+        int pos = buffer.position();
 
-        for (; buf.position() - pos < len; xs.add(read(resolveGenericType(pty.getActualTypeArguments()[0], typeMap), buf, typeMap)))
+        for (; buffer.position() - pos < length; s.add(readInput(resolveGenericType(pty.getActualTypeArguments()[0], typeMap), buffer, typeMap)))
           ;
 
-        int read = buf.position() - pos;
-        if (read != len) {
+        int read = buffer.position() - pos;
+        if (read != length) {
           throw new SerializingException(
             String.format(
               "Mismatch collection length, expected %d bytes, read %d bytes at offset %d.",
-              len,
-              len,
+              length,
               read,
-              buf.position()
+              buffer.position()
             )
           );
         }
-        return xs;
+        return s;
       }
 
-      return readStruct(ty, buf, typeMap);
+      return inputStruct(type, buffer, typeMap);
     }
 
-    Class<?> clazz = (Class) ty;
-    if (clazz.isEnum()) {
-      int i = buf.get();
-      Object[] cs = clazz.getEnumConstants();
-      if (i < 0 || i > cs.length) {
+    Class<?> currClass = (Class) type;
+    if (currClass.isEnum()) {
+      int i = buffer.get();
+      Object[] c = currClass.getEnumConstants();
+      if (i < 0 || i > c.length) {
         throw new SerializingException(
-          String.format("Invalid ordinal %d of %s at offset %d.", i, clazz.getName(), buf.position())
+          String.format("Invalid ordinal %d of %s at offset %d.", i, currClass.getName(), buffer.position())
         );
       }
 
-      return cs[i];
+      return c[i];
     }
 
-    return readStruct(ty, buf, typeMap);
+    return inputStruct(type, buffer, typeMap);
   }
 
-  public static void read(Object obj, Field field, ByteBuffer buf, Map<String, Class> typeMap) {
+  public static void read(Object obj, Field field, ByteBuffer buffer, Map<String, Class> typeMap) {
     try {
-      Type ty = field.getGenericType();
-      if (ty instanceof TypeVariable) {
-        ty = typeMap.get(((TypeVariable) ty).getName());
+      Type type = field.getGenericType();
+      if (type instanceof TypeVariable) {
+        type = typeMap.get(((TypeVariable) type).getName());
       }
-      Object x = read(ty, buf, typeMap);
-      if (ty == Byte.TYPE) {
-        field.setByte(obj, (Byte) x);
-      } else if (ty == Boolean.TYPE) {
-        field.setBoolean(obj, (Boolean) x);
-      } else if (ty == Short.TYPE) {
-        field.setShort(obj, (Short) x);
-      } else if (ty == Character.TYPE) {
-        field.setChar(obj, (Character) x);
-      } else if (ty == Integer.TYPE) {
-        field.setInt(obj, (Integer) x);
-      } else if (ty == Float.TYPE) {
-        field.setFloat(obj, (Float) x);
-      } else if (ty == Long.TYPE) {
-        field.setLong(obj, (Long) x);
-      } else if (ty == Double.TYPE) {
-        field.setDouble(obj, (Double) x);
+      Object r = readInput(type, buffer, typeMap);
+      if (type == Byte.TYPE) {
+        field.setByte(obj, (Byte) r);
+      } else if (type == Boolean.TYPE) {
+        field.setBoolean(obj, (Boolean) r);
+      } else if (type == Short.TYPE) {
+        field.setShort(obj, (Short) r);
+      } else if (type == Character.TYPE) {
+        field.setChar(obj, (Character) r);
+      } else if (type == Integer.TYPE) {
+        field.setInt(obj, (Integer) r);
+      } else if (type == Float.TYPE) {
+        field.setFloat(obj, (Float) r);
+      } else if (type == Long.TYPE) {
+        field.setLong(obj, (Long) r);
+      } else if (type == Double.TYPE) {
+        field.setDouble(obj, (Double) r);
       } else {
-        field.set(obj, x);
+        field.set(obj, r);
       }
     } catch (IllegalAccessException e) {
       assert false : "Unable to write to field " + field.getName() + " of " + obj.getClass().getName();
     }
   }
 
-  /**
-   * Makes a map from type parameters to type arguments of a `ParameterizedType`.
-   * <p>
-   * Example:
-   * <pre>{@code
-   * class Clazz<T, U, V> {}
-   *
-   * makeTypeMap((ParameterizedType) new Clazz<Integer, Boolean, Float>() {}.getClass().getGenericSuperclass(), EMPTY_TYPE_MAP);
-   * // => {T=java.lang.Integer, U=java.lang.Boolean, V=java.lang.Float}
-   * }</pre>
-   *
-   * @param pty           the type
-   * @param parentTypeMap the type map of the generic class containing this `ParameterizedType`.
-   * @return the map
-   */
+
   public static Map<String, Class> makeTypeMap(ParameterizedType pty, Map<String, Class> parentTypeMap) {
     return StreamEx.of(((Class<?>) pty.getRawType()).getTypeParameters())
       .map(TypeVariable::getName)
@@ -267,10 +226,10 @@ public class Deserializer {
       .toMap();
   }
 
-  public static Type resolveGenericType(Type ty, Map<String, Class> typeMap) {
-    if (ty instanceof TypeVariable) {
-      return typeMap.get(((TypeVariable) ty).getName());
+  public static Type resolveGenericType(Type type, Map<String, Class> typeMap) {
+    if (type instanceof TypeVariable) {
+      return typeMap.get(((TypeVariable) type).getName());
     }
-    return ty;
+    return type;
   }
 }
--- src/main/java/cz4013/common/serialization/Serializable.java
+++ src/main/java/cz4013/common/serialization/Serializable.java
@@ -2,9 +2,6 @@ package cz4013.common.serialization;
 
 import java.nio.ByteBuffer;
 
-/**
- * Interface for classes which implements custom serialization/deserialization.
- */
 public interface Serializable {
   void deserialize(ByteBuffer buf);
 
